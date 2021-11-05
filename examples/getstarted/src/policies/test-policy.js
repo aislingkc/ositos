a31883845b041cb91e91f501e4d9da5f84b31f33
Alexandre Bodin <bodin.alex@gmail.com>
2021-11-05 12:19:49 +0100

    Refactor policies to have a consistent factory API like middlewares

--- examples/getstarted/src/policies/test-policy.js
+++ examples/getstarted/src/policies/test-policy.js
@@ -1,3 +1,18 @@
-module.exports = ctx => {
-  return true;
+'use strict';
+
+/**
+ * `coucou-policy` policy.
+ */
+
+module.exports = (policyCtx, config, { strapi }) => {
+  // Add your own logic here.
+  strapi.log.info('In test-policy policy.');
+
+  const canDoSomething = true;
+
+  if (canDoSomething) {
+    return true;
+  }
+
+  return false;
 };
